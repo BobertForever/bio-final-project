37ceb23f48ef1ca89c465a07e6206c29f1323b7d
BobertForever <robertlynch2012@gmail.com>
2015-04-24 16:14:16 -0500

    Comment and cleanup in App

--- js/app.js
+++ js/app.js
@@ -1,10 +1,18 @@
 var React                 = require('react');
     Router                = require('react-router'),
     mui                   = require('material-ui'),
-    injectTapEventPlugin  = require("react-tap-event-plugin");
+    injectTapEventPlugin  = require('react-tap-event-plugin');
 
 var { AppCanvas, AppBar, Paper, LaftNav } = mui;
 var { Route, DefaultRoute, RouteHandler } = Router;
+
+/* Components */
+var About             = require('./components/about'),
+    ActivationEnergy  = require('./components/activation-energy'),
+    SubstrateDiagram  = require('./components/substrate-diagram'),
+    Ingredients       = require('./components/ingredients'),
+    InsideEnzyme      = require('./components/inside-enzyme'),
+    EnzymePolice      = require('./components/enzyme-police');
 
 /* Necessary for certain tap components to work */
 injectTapEventPlugin();
@@ -12,13 +20,41 @@ injectTapEventPlugin();
 /* React gloabal variable for React DevTools */
 window.React = React;
 
+/* Menu items and handlers */
 menuItems = [
-  { route: 'what-are-enzymes', text: 'What are Enzymes?', handler: About },
-  { route: 'enzymes-and-activation-energy', text: 'Enzymes and Activation Energy', handler: ActivationEnergy },
-  { route: 'substrate-diagram', text: 'Substrate Diagram', handler: SubstrateDiagram },
-  { route: 'enzyme-ingredients', text: 'Enzyme Ingredients', handler: Ingredients }
+  {
+    route: 'what-are-enzymes',
+    text: 'What are Enzymes?',
+    handler: About
+  },
+  {
+    route: 'enzymes-and-activation-energy',
+    text: 'Enzymes and Activation Energy',
+    handler: ActivationEnergy
+  },
+  {
+    route: 'substrate-diagram',
+    text: 'Substrate Diagram',
+    handler: SubstrateDiagram
+  },
+  {
+    route: 'enzyme-ingredients',
+    text: 'Enzyme Ingredients',
+    handler: Ingredients
+  },
+  {
+    route: 'inside-the-enzyme',
+    test: 'Inside the Enzyme',
+    handler: InsideEnzyme
+  },
+  {
+    route: 'enzyme-police',
+    text: 'Enzyme Police: Regulation of Enzyme Activity',
+    handler: EnzymePolice
+  }
 ];
 
+/* Master handler */
 class Master extends React.Component {
 
   render() {
@@ -39,6 +75,7 @@ class Master extends React.Component {
   }
 }
 
+/* Setup the routes */
 var appRoutes = (
   <Route name="root" path="/" handler={Master}>
     {menuItems.map(function(result) {
@@ -49,6 +86,7 @@ var appRoutes = (
   </Route>
 );
 
+/* Render the React app */
 Router.run(appRoutes, function(Handler) {
   React.render(<Handler />, document.getElementById('content'))
 });
--- js/components/about.js
+++ js/components/about.js
@@ -1,5 +1,9 @@
 var React = require('React');
 
 class About extends React.Component {
+  render() {
 
+  }
 }
+
+module.exports = About;
