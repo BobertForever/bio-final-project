9f6c497f3cc87086c0475791c16921fe0e4f1814
BobertForever <robertlynch2012@gmail.com>
2015-04-27 14:22:23 -0500

    Added more content

--- js/components/activation-energy.js
+++ js/components/activation-energy.js
@@ -11,11 +11,28 @@ class ActivationEnergy extends React.Component {
         of the reactants as a boulder sitting on top of a hill. The ball has potential energy and
         favors rolling down the hill. It won't move, however, until it is given a push to get it
         rolling.</p>
+        <img src='img/img1.gif' />
         <p>Similarly, an enzyme lowers the activation energy of a reaction by binding to substrates
         and providing the proper environment for reactions to proceed. It is important to note that
         enzymes do not change the overall Free Energy of a reaction. The reactions take place
         without enzymes but with greatly decreased frequency because of the random motion of
         molecules.</p>
+        <img src='img/img2.jpg' />
+        <p>Enzymes increase the rate of these reactions and are therefore, important in every
+        complex metabolic pathway. Take, for example, cellular respiration. This process is,
+        overall, an exergonic one. Imagine, however, trying to convert the food you eat into
+        chemical energy your body can use without enzymes. The number of steps in this pathway
+        would make this kind of spontaneous reaction virtually impossible because the rate of
+        reaction would be infinitely small compared to the rate of consumption. Enzymes accomplish
+        this important role by specifically binding to certain substrates (or close relatives) at
+        the active site. The enzymes "hug" the substrates in a form called induced fit.</p>
+        <img src="img/img3.jpg" />
+        <p>Here, the side chains of the enzyme can interact with the substrate to facilitate the
+        reaction.</p>
+        <hr/>
+        <h3>Some Helpful Videos</h3>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/j00Ep0Byu0Y"></iframe>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/YacsIU97OFc"></iframe>
       </div>
     );
   }
--- js/components/ingredients.js
+++ js/components/ingredients.js
@@ -3,7 +3,31 @@ var React = require('React');
 class Ingredients extends React.Component {
   render() {
     return (
-      <p>Hello, World!</p>
+      <div>
+        <h2>Enzyme Ingredients: Factors Affecting Enzyme Activity</h2>
+        <p><b>Enzyme Concentration</b>: In order to study the effect of increasing enzyme
+        concentration on the reaction rate, the reaction must be independent of the substrate
+        concentration. The level of the enzyme present will dictate how much product is formed, as
+        illustrated by the graph.</p>
+        <img src='img/img6.png' />
+        <p><b>Substrate Concentration</b>: Each enzyme needs the right kind of substrate(s) to
+        perform the reaction. If the amount of enzyme is kept constant and the substrate
+        concentration is increased, the reaction velocity will increase until it reaches a maximum,
+        as shown with in the graph.</p>
+        <img src='img/img7.png' />
+        <p><b>Temperature</b>: Each enzyme has an optimum temperature for maximum activity. In the
+        human body, the optimum temperature for enzymes is 37°C. In bacteria, the optimum
+        temperature for enzymes is 72°C.  The reaction rate increases with temperature to a maximum
+        level, then quickly declines with further increase of temperature. Over a period of time,
+        enzymes will be deactivated at even moderate temperatures.</p>
+        <img src='img/img8.png' />
+        <p><b>pH</b>: Enzymes require an optimum pH to operate. Extremely high or low pH values
+        generally result in complete loss of activity for most enzymes.</p>
+        <img src='img/img9.png' />
+        <p><b>Salt</b>: Enzymes require the presence of certain ions or salts at certain
+        concentrations to catalyze reactions.</p>
+        <img src='img/img10.png' />
+      </div>
     );
   }
 }
